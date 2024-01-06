7aeeaf1a90ddbc0c79cf7f473faf7eb9d38a1c2f
aarushisingh2004 <120239056+aarushisingh2004@users.noreply.github.com>
2024-01-06 12:15:18 +0530

    reveal hidden  content

--- app.js
+++ app.js
@@ -29,3 +29,14 @@ newBackgroundColor.addEventListener("mouseover",changeBgColor);
 const revealBtn=document.querySelector(".reveal-btn");
 
 //start from here
+const hiddenContent=document.querySelector(".hidden-content");
+
+function revealContent(){
+    if(hiddenContent.classList.contains('reveal-btn')){
+       hiddenContent.classList.remove('reveal-btn');        
+    }
+    else{
+       hiddenContent.classList.add('reveal-btn'); 
+    }
+}
+revealBtn.addEventListener("click", revealContent);
